9e41f95b67561b91781264fb6ef51e6ae2fb3da0
Fabio Ewerton <fabioew89@gmail.com>
2017-07-22 02:15:02 -0400

    adicionado arquivos básicos do projeto

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('html', function() {
+    return gulp.src('_source/index.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('_dist/'));
+});
+
+
+
+gulp.task('default', function() {
+	gulp.watch('_source/index.html', ['html']);    
+
+});	
