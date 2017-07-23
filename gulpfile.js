c19b419448c0e6adf1378a9baed65907c8b96185
Fabio Ewerton <fabioew89@gmail.com>
2017-07-23 15:46:51 -0300

    gulpfile.js ajustado

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,19 @@ var notify = require('gulp-notify');
 
 //Copia e minifica todos os arquivos Html do _source/ para o _dist/
 gulp.task('html', function() {
-    return gulp.src('_source/*.html')
-	    .pipe(htmlmin({collapseWhitespace: true}))
-    	.pipe(gulp.dest('_dist/'));
+    return gulp.src('_source/index.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('_dist/'));
 });
 
-//Copia, Compila, Transfere e Verifica possiveis erros.
 gulp.task('sass', function() {
-    return gulp.src('_source/scss/style.scss')
+    return gulp.src('_source/scss/**/*.scss')
       .pipe(sass())
       .on("error", notify.onError({title: "OPA... TEM ALGUMA COISA ERRADA COM SEU CÓDIGO, VOCÊ DEVERIA DAR UMA OLHADA", message: "Error: <%= error.message %>"}))
       .pipe(cleanCss())
       .pipe(gulp.dest('_dist/css/'));
 });
 
-//Tarefa padrão do gulp
 gulp.task('default', function() {
     gulp.watch('_source/index.html', ['html']);
     gulp.watch('_source/scss/**/*.scss', ['sass']);
